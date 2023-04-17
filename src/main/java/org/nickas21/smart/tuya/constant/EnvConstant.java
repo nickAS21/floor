f6d4ee007e0b0e637441912166790ff91e16db6e
nickAS21 <nick@thingsboard.io>
2023-04-17 10:27:02 +0300

    add MessageFactory

---
+++ src/main/java/org/nickas21/smart/tuya/constant/EnvConstant.java
@@ -0,0 +1,12 @@
+package org.nickas21.smart.tuya.constant;
+
+public class EnvConstant {
+
+    public static final String ENV_AK = "connector.ak";
+
+    public static final String ENV_SK = "connector.sk";
+
+    public static final String ENV_REGION = "connector.region";
+
+
+}
