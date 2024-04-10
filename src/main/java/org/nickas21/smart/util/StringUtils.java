9a28de695c0d4ca2f7b940a8d617d5f03e6942ee
nick <nick@avalr.com.ua>
2024-04-10 23:43:53 +0300

    refactoring progress  bar utf-8

--- src/main/java/org/nickas21/smart/util/StringUtils.java
+++ src/main/java/org/nickas21/smart/util/StringUtils.java
@@ -14,13 +14,17 @@ public class StringUtils {
     public static void printMsgWithProgressBar(String message, long timeAll) {
         long timeInterval = 5000L;
         int length = Math.toIntExact(timeAll/timeInterval);
-        char incomplete = '░'; // U+2591 Unicode Character
-        char complete = '█'; // U+2588 Unicode Character
+//        char incomplete = '░'; // U+2591 Unicode Character
+//        char complete = '█'; // U+2588 Unicode Character
+         char incomplete = '.'; // U+2591 Unicode Character
+        char complete = '='; // U+2588 Unicode Character
+        char completeFinish = '>'; // U+2588 Unicode Character
         StringBuilder builder = new StringBuilder();
         Stream.generate(() -> incomplete).limit(length).forEach(builder::append);
 //        System.out.println(message);
         for (int i = 0; i < length; i++) {
             builder.replace(i, i + 1, String.valueOf(complete));
+            builder.replace(i+1, i +2, String.valueOf(completeFinish));
             String progressBar = "\r" + message + builder + "\r" ;
             System.out.print(progressBar);
             try {
