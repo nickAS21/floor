5f3d910626e5c7c92d6876610cbac44b0369bba6
nick <nick@avalr.com.ua>
2024-04-11 12:00:18 +0300

    refactoring progress bar old

--- src/main/java/org/nickas21/smart/DefaultSmartSolarmanTuyaService.java
+++ src/main/java/org/nickas21/smart/DefaultSmartSolarmanTuyaService.java
@@ -37,7 +37,6 @@ import static org.nickas21.smart.util.HttpUtil.totalEnergyBuyKey;
 import static org.nickas21.smart.util.HttpUtil.totalEnergySellKey;
 import static org.nickas21.smart.util.HttpUtil.totalGridPowerKey;
 import static org.nickas21.smart.util.HttpUtil.totalSolarPowerKey;
-import static org.nickas21.smart.util.StringUtils.printMsgProgressBar;
 import static org.nickas21.smart.util.StringUtils.printMsgWithProgressBar;
 
 @Slf4j
@@ -86,10 +85,8 @@ public class DefaultSmartSolarmanTuyaService implements SmartSolarmanTuyaService
             String batteryPowerNewStr = -batteryPowerNew + " W";
             Instant curInst = Instant.now();
             String curInstStr = toLocaleTimeString(curInst);
-            printMsgProgressBar(curInstStr + ". Next update: " + toLocaleTimeString(Instant.ofEpochMilli(curInst.toEpochMilli() + solarmanStationsService.getSolarmanStation().getTimeoutSec()*1000)) + ",  after [" + solarmanStationsService.getSolarmanStation().getTimeoutSec()/60 + "] min: ",
+            printMsgWithProgressBar(curInstStr + ". Next update: " + toLocaleTimeString(Instant.ofEpochMilli(curInst.toEpochMilli() + solarmanStationsService.getSolarmanStation().getTimeoutSec()*1000)) + ",  after [" + solarmanStationsService.getSolarmanStation().getTimeoutSec()/60 + "] min: ",
                     solarmanStationsService.getSolarmanStation().getTimeoutSec()*1000);
-//            printMsgWithProgressBar(curInstStr + ". Next update: " + toLocaleTimeString(Instant.ofEpochMilli(curInst.toEpochMilli() + solarmanStationsService.getSolarmanStation().getTimeoutSec()*1000)) + ",  after [" + solarmanStationsService.getSolarmanStation().getTimeoutSec()/60 + "] min: ",
-//                    solarmanStationsService.getSolarmanStation().getTimeoutSec()*1000);
             if (this.batterySocCur == 0) {
                 try {
                     this.tuyaDeviceService.updateAllThermostat(this.tuyaDeviceService.getDeviceProperties().getTempSetMin());
--- src/main/java/org/nickas21/smart/util/StringUtils.java
+++ src/main/java/org/nickas21/smart/util/StringUtils.java
@@ -10,21 +10,6 @@ public class StringUtils {
     public static boolean isBlank(String source) {
         return source == null || source.isEmpty() || source.trim().isEmpty();
     }
-
-    public static void printMsgProgressBar(String message, long timeAll) {
-        long timeInterval = 5000L;
-        int size = Math.toIntExact(timeAll/timeInterval);
-        for (int i=1; i<size; i++) {
-            try {
-                Thread.sleep(timeInterval);
-                System.out.print(message + "[" + "=".repeat(i) + ">" + " ".repeat(size-i) + "]\r");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.print("|" + "=".repeat(size) + "|\r");
-
-    }
     public static void printMsgWithProgressBar(String message, long timeAll) {
         long timeInterval = 5000L;
         int length = Math.toIntExact(timeAll/timeInterval);
