0208dcc2d7f1b727edcaaa20a553bd329f62577a
nick <nick@avalr.com.ua>
2024-04-13 08:31:02 +0300

    logger - setDefault(TimeZone.getTimeZone("Europe/Kyiv")

--- src/main/java/org/nickas21/smart/SmartSolarmanTuyaApplication.java
+++ src/main/java/org/nickas21/smart/SmartSolarmanTuyaApplication.java
@@ -4,11 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @Slf4j
 @SpringBootApplication
 public class SmartSolarmanTuyaApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Kyiv"));
         SpringApplication.run(SmartSolarmanTuyaApplication.class);
     }
 
