f7e8323a5dde14afee74f6c1b1db3603375db665
nick <nick@avalr.com.ua>
2024-04-14 13:42:07 +0300

    update TimeoutSec ==3/2

--- src/main/java/org/nickas21/smart/DefaultSmartSolarmanTuyaService.java
+++ src/main/java/org/nickas21/smart/DefaultSmartSolarmanTuyaService.java
@@ -303,7 +303,8 @@ public class DefaultSmartSolarmanTuyaService implements SmartSolarmanTuyaService
     private void setTimeoutSecUpdate() {
         if (isDay) {
             if ( this.sunRiseMax != null && this.curDate.toEpochMilli() < this.sunRiseMax) {
-                this.timeoutSecUpdate = solarmanStationsService.getSolarmanStation().getTimeoutSec() * (3/2); // 6 min
+
+                this.timeoutSecUpdate = (solarmanStationsService.getSolarmanStation().getTimeoutSec() * 3)/2; // 3 min
             } else {
                 this.timeoutSecUpdate = solarmanStationsService.getSolarmanStation().getTimeoutSec() * 2;   // 4 min
             }
