93ca8ed0127d1be7f7c686803027ab9114690959
nick <nick@avalr.com.ua>
2024-06-13 12:49:04 +0300

    translate: msgTelegram to English

--- src/main/java/org/nickas21/smart/security/configuration/TelegramBotConfig.java
+++ src/main/java/org/nickas21/smart/security/configuration/TelegramBotConfig.java
@@ -1,17 +1,20 @@
 package org.nickas21.smart.security.configuration;
 
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.nickas21.smart.data.entity.TelegramBot;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
 @Configuration
-public class TelegramBotConfig {
+public class TelegramBotConfig  implements DisposableBean {
 
     @Value("${telegram.bot.username}")
     private String botUsername;
@@ -19,19 +22,23 @@ public class TelegramBotConfig {
     @Value("${telegram.bot.token}")
     private String botToken;
 
+    private TelegramBotsApi botsApi;
+    private TelegramBot bot;
+    private BotSession botSession;
+
     @Bean
     public TelegramBot telegramBot() {
-        TelegramBot bot = new TelegramBot();
+        bot = new TelegramBot();
         bot.setBotUsername(botUsername);
         bot.setBotToken(botToken);
         return bot;
     }
 
     @Bean
-    public TelegramBotsApi telegramBotsApi(TelegramBot bot)  {
+    public TelegramBotsApi telegramBotsApi(TelegramBot bot) {
         try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(bot);
+            botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botSession =  botsApi.registerBot(bot);
             bot.setStateStart(true);
             log.info("TelegramBotsApi is started successful.");
             return botsApi;
@@ -39,7 +46,20 @@ public class TelegramBotConfig {
             log.error("TelegramBotsApi is null. BotUsername: [{}] BotToken: [{}] error: [{}]", bot.getBotUsername(), bot.getBotToken(), e.getMessage());
             return null;
         }
+    }
 
+    @PreDestroy
+    @Override
+    public void destroy() {
+        if (bot.isStateStart()) {
+            try {
+                botSession.stop();
+                bot.onClosing();
+                botsApi = null;
+                log.info("Telegram bot unregistered successfully.");
+            } catch (Exception e) {
+                log.error("Failed to unregister bot: {}", e.getMessage());
+            }
+        }
     }
 }
-
